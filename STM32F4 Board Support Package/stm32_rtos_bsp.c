38df587c4c639e9abb83eb08bc9e86e1bcde43c1
Gabe <gabe.teeger@gmail.com>
2020-04-13 00:34:40 -0400

    proper code alignment

--- STM32F4 Board Support Package/stm32_rtos_bsp.c
+++ STM32F4 Board Support Package/stm32_rtos_bsp.c
@@ -2,354 +2,335 @@
 
 //*******************************Start of TIMERS APIs**************/
 
-
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
 TIM_HandleTypeDef htim5;
 
-/* TIM2 init function */
+/*TIM2 init function */
 void TIM2_Init(void)
 {
-  TIM_ClockConfigTypeDef sClockSourceConfig;
-  TIM_MasterConfigTypeDef sMasterConfig;
+	TIM_ClockConfigTypeDef sClockSourceConfig;
+	TIM_MasterConfigTypeDef sMasterConfig;
 
-  htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 16000; // Scale down to milliseconds
-  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 1;   //ms
-  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  
+	htim2.Instance = TIM2;
+	htim2.Init.Prescaler = 16000;	// Scale down to milliseconds
+	htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim2.Init.Period = 1;	//ms
+	htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+
 	HAL_TIM_Base_Init(&htim2);
-  
-  
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig);
- 
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  
+
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig);
+
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+
 	HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig);
 
 }
-/* TIM3 init function */
+/*TIM3 init function */
 void TIM3_Init(void)
 {
-  TIM_ClockConfigTypeDef sClockSourceConfig;
-  TIM_MasterConfigTypeDef sMasterConfig;
+	TIM_ClockConfigTypeDef sClockSourceConfig;
+	TIM_MasterConfigTypeDef sMasterConfig;
 
-  htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 16000;
-  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim3.Init.Period = 100;  //ms
-  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  HAL_TIM_Base_Init(&htim3);
+	htim3.Instance = TIM3;
+	htim3.Init.Prescaler = 16000;
+	htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim3.Init.Period = 100;	//ms
+	htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	HAL_TIM_Base_Init(&htim3);
 
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+
 	HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig);
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig);
-  
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig);
 
 }
-/* TIM4 init function */
+/*TIM4 init function */
 void TIM4_Init(void)
 {
-  TIM_ClockConfigTypeDef sClockSourceConfig;
-  TIM_MasterConfigTypeDef sMasterConfig;
+	TIM_ClockConfigTypeDef sClockSourceConfig;
+	TIM_MasterConfigTypeDef sMasterConfig;
 
-  htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 16000;
-  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 1;   //ms
-  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  HAL_TIM_Base_Init(&htim4);
- 
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig);
- 
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig);
- 
+	htim4.Instance = TIM4;
+	htim4.Init.Prescaler = 16000;
+	htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim4.Init.Period = 1;	//ms
+	htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	HAL_TIM_Base_Init(&htim4);
+
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig);
+
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig);
+
 }
-/* TIM5 init function */
+/*TIM5 init function */
 void TIM5_Init(void)
 {
-  TIM_ClockConfigTypeDef sClockSourceConfig;
-  TIM_MasterConfigTypeDef sMasterConfig;
+	TIM_ClockConfigTypeDef sClockSourceConfig;
+	TIM_MasterConfigTypeDef sMasterConfig;
 
-  htim5.Instance = TIM5;
-  htim5.Init.Prescaler = 16000;   // Scale down to microseconds
-  htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim5.Init.Period = 1;  // us
-  htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  HAL_TIM_Base_Init(&htim5);
+	htim5.Instance = TIM5;
+	htim5.Init.Prescaler = 16000;	// Scale down to microseconds
+	htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htim5.Init.Period = 1;	// us
+	htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	HAL_TIM_Base_Init(&htim5);
 
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  HAL_TIM_ConfigClockSource(&htim5, &sClockSourceConfig);
- 
+	sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+	HAL_TIM_ConfigClockSource(&htim5, &sClockSourceConfig);
 
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  HAL_TIMEx_MasterConfigSynchronization(&htim5, &sMasterConfig);
-  
+	sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+	sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+	HAL_TIMEx_MasterConfigSynchronization(&htim5, &sMasterConfig);
+
 }
-void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *tim_baseHandle)
 {
 
-  if(tim_baseHandle->Instance==TIM2)
-  {
- 
-    /* Peripheral clock enable */
-    __HAL_RCC_TIM2_CLK_ENABLE();
+	if (tim_baseHandle->Instance == TIM2)
+	{
 
-    /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM2_IRQn);
-  }
-  else if(tim_baseHandle->Instance==TIM3)
-  {
-    /* Peripheral clock enable */
-    __HAL_RCC_TIM3_CLK_ENABLE();
+		/*Peripheral clock enable */
+		__HAL_RCC_TIM2_CLK_ENABLE();
 
-    /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM3_IRQn);
+		/*Peripheral interrupt init */
+		HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(TIM2_IRQn);
+	}
+	else if (tim_baseHandle->Instance == TIM3)
+	{
+		/*Peripheral clock enable */
+		__HAL_RCC_TIM3_CLK_ENABLE();
 
-  }
-  else if(tim_baseHandle->Instance==TIM4)
-  {
+		/*Peripheral interrupt init */
+		HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(TIM3_IRQn);
+	}
+	else if (tim_baseHandle->Instance == TIM4)
+	{
 
-    /* Peripheral clock enable */
-    __HAL_RCC_TIM4_CLK_ENABLE();
+		/*Peripheral clock enable */
+		__HAL_RCC_TIM4_CLK_ENABLE();
 
-    /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(TIM4_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM4_IRQn);
- 
-  }
-  else if(tim_baseHandle->Instance==TIM5)
-  {
- 
-    /* Peripheral clock enable */
-    __HAL_RCC_TIM5_CLK_ENABLE();
+		/*Peripheral interrupt init */
+		HAL_NVIC_SetPriority(TIM4_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(TIM4_IRQn);
+	}
+	else if (tim_baseHandle->Instance == TIM5)
+	{
 
-    /* Peripheral interrupt init */
-    HAL_NVIC_SetPriority(TIM5_IRQn, 0, 0);
-    HAL_NVIC_EnableIRQ(TIM5_IRQn);
-  
-  }
+		/*Peripheral clock enable */
+		__HAL_RCC_TIM5_CLK_ENABLE();
+
+		/*Peripheral interrupt init */
+		HAL_NVIC_SetPriority(TIM5_IRQn, 0, 0);
+		HAL_NVIC_EnableIRQ(TIM5_IRQn);
+	}
 }
 
-void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *tim_baseHandle)
 {
 
-  if(tim_baseHandle->Instance==TIM2)
-  {
-  
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM2_CLK_DISABLE();
+	if (tim_baseHandle->Instance == TIM2)
+	{
 
-    /* Peripheral interrupt Deinit*/
-    HAL_NVIC_DisableIRQ(TIM2_IRQn);
+		/*Peripheral clock disable */
+		__HAL_RCC_TIM2_CLK_DISABLE();
 
+		/*Peripheral interrupt Deinit*/
+		HAL_NVIC_DisableIRQ(TIM2_IRQn);
+	}
+	else if (tim_baseHandle->Instance == TIM3)
+	{
 
-  }
-  else if(tim_baseHandle->Instance==TIM3)
-  {
- 
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM3_CLK_DISABLE();
+		/*Peripheral clock disable */
+		__HAL_RCC_TIM3_CLK_DISABLE();
 
-    /* Peripheral interrupt Deinit*/
-    HAL_NVIC_DisableIRQ(TIM3_IRQn);
+		/*Peripheral interrupt Deinit*/
+		HAL_NVIC_DisableIRQ(TIM3_IRQn);
+	}
+	else if (tim_baseHandle->Instance == TIM4)
+	{
 
-  }
-  else if(tim_baseHandle->Instance==TIM4)
-  {
- 
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM4_CLK_DISABLE();
+		/*Peripheral clock disable */
+		__HAL_RCC_TIM4_CLK_DISABLE();
 
-    /* Peripheral interrupt Deinit*/
-    HAL_NVIC_DisableIRQ(TIM4_IRQn);
+		/*Peripheral interrupt Deinit*/
+		HAL_NVIC_DisableIRQ(TIM4_IRQn);
+	}
+	else if (tim_baseHandle->Instance == TIM5)
+	{
 
-  
-  }
-  else if(tim_baseHandle->Instance==TIM5)
-  {
-  
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM5_CLK_DISABLE();
+		/*Peripheral clock disable */
+		__HAL_RCC_TIM5_CLK_DISABLE();
 
-    /* Peripheral interrupt Deinit*/
-    HAL_NVIC_DisableIRQ(TIM5_IRQn);
-
-
-  }
-} 
+		/*Peripheral interrupt Deinit*/
+		HAL_NVIC_DisableIRQ(TIM5_IRQn);
+	}
+}
 void TIM2_Init_Start(void)
 {
-	 TIM2_Init();
-	
-   HAL_TIM_Base_Start_IT(&htim2);
+	TIM2_Init();
+
+	HAL_TIM_Base_Start_IT(&htim2);
 
 }
 
 void TIM3_Init_Start(void)
 {
-	 TIM3_Init();
-	
-   HAL_TIM_Base_Start_IT(&htim3);
+	TIM3_Init();
+
+	HAL_TIM_Base_Start_IT(&htim3);
 
 }
 void TIM4_Init_Start(void)
 {
-	 TIM4_Init();
-	
-   HAL_TIM_Base_Start_IT(&htim4);
+	TIM4_Init();
+
+	HAL_TIM_Base_Start_IT(&htim4);
 
 }
 void TIM5_Init_Start(void)
 {
-	 TIM5_Init();
-	
-   HAL_TIM_Base_Start_IT(&htim5);
+	TIM5_Init();
+
+	HAL_TIM_Base_Start_IT(&htim5);
 
 }
 
-int counter2,counter3,counter4,counter5;
+int counter2, counter3, counter4, counter5;
 
 /**
-* @brief This function handles TIM2 global interrupt.
-*/
+ *@brief This function handles TIM2 global interrupt.
+ */
 void TIM2_IRQHandler(void)
 {
-  
+
 	counter2++;
 	HAL_IncTick();
-  HAL_TIM_IRQHandler(&htim2);
- 
+	HAL_TIM_IRQHandler(&htim2);
+
 }
 
 /**
-* @brief This function handles TIM3 global interrupt.
-*/
+ *@brief This function handles TIM3 global interrupt.
+ */
 void TIM3_IRQHandler(void)
 {
 	//counter3++;
- 	//HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15);
+	//HAL_GPIO_TogglePin(GPIOD,GPIO_PIN_15);
 	Probe_CH0();
-  HAL_TIM_IRQHandler(&htim3);
-  
+	HAL_TIM_IRQHandler(&htim3);
+
 }
 
 /**
-* @brief This function handles TIM4 global interrupt.
-*/
+ *@brief This function handles TIM4 global interrupt.
+ */
 void TIM4_IRQHandler(void)
 {
-  counter4++;
-  HAL_TIM_IRQHandler(&htim4);
+	counter4++;
+	HAL_TIM_IRQHandler(&htim4);
 
 }
 
 /**
-* @brief This function handles TIM5 global interrupt.
-*/
+ *@brief This function handles TIM5 global interrupt.
+ */
 void TIM5_IRQHandler(void)
 {
-  counter5++;
-  HAL_TIM_IRQHandler(&htim5);
- 
+	counter5++;
+	HAL_TIM_IRQHandler(&htim5);
+
 }
 //------------Milliseconds delay using TIMER2-------------//
- void DelayMs(__IO uint32_t Delay)
+void DelayMs(__IO uint32_t Delay)
 {
-  uint32_t tickstart = 0U;
-  tickstart = HAL_GetTick();
-  while((HAL_GetTick() - tickstart) < Delay){}
-  
+	uint32_t tickstart = 0 U;
+	tickstart = HAL_GetTick();
+	while ((HAL_GetTick() - tickstart) < Delay) {}
 }
 
 //**********************Start of ADC APIs**************************//
 ADC_HandleTypeDef hadc1;
 
- void ADC1_Init(void)
+void ADC1_Init(void)
 {
 	__HAL_RCC_GPIOH_CLK_ENABLE();
-  __HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 
-  ADC_ChannelConfTypeDef sConfig;
+	ADC_ChannelConfTypeDef sConfig;
 
-    /**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
-    */
-  hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
-  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
-  hadc1.Init.ScanConvMode = DISABLE;
-  hadc1.Init.ContinuousConvMode = DISABLE;
-  hadc1.Init.DiscontinuousConvMode = ENABLE;
-  hadc1.Init.NbrOfDiscConversion = 1;
-  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
-  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.NbrOfConversion = 1;
-  hadc1.Init.DMAContinuousRequests = DISABLE;
-  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
- 
+	/**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+	 */
+	hadc1.Instance = ADC1;
+	hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+	hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+	hadc1.Init.ScanConvMode = DISABLE;
+	hadc1.Init.ContinuousConvMode = DISABLE;
+	hadc1.Init.DiscontinuousConvMode = ENABLE;
+	hadc1.Init.NbrOfDiscConversion = 1;
+	hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	hadc1.Init.NbrOfConversion = 1;
+	hadc1.Init.DMAContinuousRequests = DISABLE;
+	hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+
 	HAL_ADC_Init(&hadc1);
 
-    /**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
-    */
-  sConfig.Channel = ADC_CHANNEL_4;
-  sConfig.Rank = 1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
-  HAL_ADC_ConfigChannel(&hadc1, &sConfig);
- 
-}
-
-
-void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
-{
-
-  GPIO_InitTypeDef GPIO_InitStruct;
-  if(hadc->Instance==ADC1)
-  {
-
-    /* Peripheral clock enable */
-    __HAL_RCC_ADC1_CLK_ENABLE();
-  
-    /**ADC1 GPIO Configuration    
-    PA4     ------> ADC1_IN4 
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_4;
-    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-  }
+	/**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+	 */
+	sConfig.Channel = ADC_CHANNEL_4;
+	sConfig.Rank = 1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+	HAL_ADC_ConfigChannel(&hadc1, &sConfig);
 
 }
 
-void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 {
 
-  if(hadc->Instance==ADC1)
-  {
+	GPIO_InitTypeDef GPIO_InitStruct;
+	if (hadc->Instance == ADC1)
+	{
 
-    /* Peripheral clock disable */
-    __HAL_RCC_ADC1_CLK_DISABLE();
-  
-    /**ADC1 GPIO Configuration    
-    PA4     ------> ADC1_IN4 
-    */
-    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_4);
+		/*Peripheral clock enable */
+		__HAL_RCC_ADC1_CLK_ENABLE();
 
-  }
- 
+		/**ADC1 GPIO Configuration    
+		PA4     ------> ADC1_IN4 
+		*/
+		GPIO_InitStruct.Pin = GPIO_PIN_4;
+		GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+		GPIO_InitStruct.Pull = GPIO_NOPULL;
+		HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	}
+}
+
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
+{
+
+	if (hadc->Instance == ADC1)
+	{
+
+		/*Peripheral clock disable */
+		__HAL_RCC_ADC1_CLK_DISABLE();
+
+		/**ADC1 GPIO Configuration    
+		PA4     ------> ADC1_IN4 
+		*/
+		HAL_GPIO_DeInit(GPIOA, GPIO_PIN_4);
+	}
 }
 //**********************End of ADC APIs**************************//
 
@@ -358,35 +339,34 @@ GPIO_InitTypeDef GPIO_InitStruct;
 
 void Probe_Init(void)
 {
-	
+
 	__HAL_RCC_GPIOC_CLK_ENABLE();
 
-  HAL_GPIO_WritePin(PROBE_PORT, CH0|CH1|CH2|CH3, GPIO_PIN_RESET);
-  
-	
-	GPIO_InitStruct.Pin    = CH0|CH1|CH2|CH3;
-  GPIO_InitStruct.Mode   = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull   = GPIO_NOPULL;
-	GPIO_InitStruct.Speed  = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_WritePin(PROBE_PORT, CH0 | CH1 | CH2 | CH3, GPIO_PIN_RESET);
+
+	GPIO_InitStruct.Pin = CH0 | CH1 | CH2 | CH3;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(PROBE_PORT, &GPIO_InitStruct);
 
 }
 void Probe_CH0(void)
 {
-	HAL_GPIO_TogglePin(PROBE_PORT,CH0);
+	HAL_GPIO_TogglePin(PROBE_PORT, CH0);
 }
 
 void Probe_CH1(void)
 {
-	HAL_GPIO_TogglePin(PROBE_PORT,CH1);
+	HAL_GPIO_TogglePin(PROBE_PORT, CH1);
 }
 void Probe_CH2(void)
 {
-	HAL_GPIO_TogglePin(PROBE_PORT,CH2);
+	HAL_GPIO_TogglePin(PROBE_PORT, CH2);
 }
 void Probe_CH3(void)
 {
-	HAL_GPIO_TogglePin(PROBE_PORT,CH3);
+	HAL_GPIO_TogglePin(PROBE_PORT, CH3);
 }
 
 //********************End of Systick APIs*******************************/
@@ -395,44 +375,38 @@ void Probe_CH3(void)
 void SystemClock_Config80Mhz(void)
 {
 
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+	RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC_ClkInitTypeDef RCC_ClkInitStruct;
 
-    /**Configure the main internal regulator output voltage 
-    */
-  __HAL_RCC_PWR_CLK_ENABLE();
+	/**Configure the main internal regulator output voltage 
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
 
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 80;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 8;
-  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	/**Initializes the CPU, AHB and APB busses clocks 
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = 16;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 80;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 8;
+	HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
-    /**Initializes the CPU, AHB and APB busses clocks 
-    */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+	/**Initializes the CPU, AHB and APB busses clocks 
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+		RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
- HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
-  
- 
+	HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2);
 
 }
 //*******************************End of SystemClockConfig APIs**************/
-
-
-
-
